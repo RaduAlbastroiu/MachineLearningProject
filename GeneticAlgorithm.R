9d8166f62819135d83f054a70dd73ef0615a8f00
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-03-18 16:27:38 +0000

    Tried genetic algorithms for on a function

---
+++ GeneticAlgorithm.R
@@ -0,0 +1,15 @@
+# genetic algorithm
+
+library("GA")
+library("foreach")
+library("iterators")
+
+
+Rastrigin <- function(x1, x2)
+{
+  20 + x1^2 + x2^2 - 10*(cos(2*pi*x1) + cos(2*pi*x2))
+}
+
+x1 <- x2 <- seq(-5.12, 5.12, by = 0.1)
+f <- outer(x1, x2, Rastrigin)
+persp3D(x1, x2, f, theta = 50, phi = 20, color.palette = bl2gr.colors)
