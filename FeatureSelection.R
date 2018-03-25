8e0f095bd25e8d61c6c158bfc604490469357ca8
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-03-25 11:56:56 +0100

    Keeping the unique feature comb

--- FeatureSelection.R
+++ FeatureSelection.R
@@ -12,5 +12,10 @@ source('FeatureSelectionWrapper.R')
 # embedded methods
 source('FeatureSelectionEmbedded.R')
 
+# sort values to eliminate duplicates
+for(i in 1:length(feature.selection.list)) {
+  feature.selection.list[[i]] <- sort(feature.selection.list[[i]])
+}
+
 # keep unique elements only
 feature.selection.list <- unique(feature.selection.list)
