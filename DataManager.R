2f0fecd6435bf16545fe8647898d7129863b24ca
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-03-27 06:47:27 +0100

    Dataset list updated

--- DataManager.R
+++ DataManager.R
@@ -14,8 +14,11 @@ source('DataSplitter.R')
 
 # returns the name of the dataset from datasets.list
 datasetsNames = function(first.index, second.index) {
-  if(first.index > 2)
-  {
+
+    if(first.index == 1)
+      return('Simple data')
+    if(first.index == 2)
+      return('C1Stress data')
     if(first.index == 3)
       return('Oversampled data')
     if(first.index == 4)
@@ -27,46 +30,34 @@ datasetsNames = function(first.index, second.index) {
     if(first.index == 7)
       return('Smote data')
     if(first.index == 8)
-      return('Normalized Oversampled data')
+      return('Normalized Simple data')
     if(first.index == 9)
-      return('Normalized Undersampled data')
+      return('Normalized C1Stress data')
     if(first.index == 10)
-      return('Normalized Hybrid data')
+      return('Normalized Oversampled data')
     if(first.index == 11)
-      return('Normalized Rose data')
+      return('Normalized Undersampled data')
     if(first.index == 12)
-      return('Normalized Smote data')
+      return('Normalized Hybrid data')
     if(first.index == 13)
-      return('Scaled Oversampled data')
+      return('Normalized Rose data')
     if(first.index == 14)
-      return('Scaled Undersampled data')
+      return('Normalized Smote data')
     if(first.index == 15)
-      return('Scaled Hybrid data')
+      return("Scaled Simple date")
     if(first.index == 16)
-      return('Scaled Rose data')
+      return("Scaled C1Stress data")
     if(first.index == 17)
+      return('Scaled Oversampled data')
+    if(first.index == 18)
+      return('Scaled Undersampled data')
+    if(first.index == 19)
+      return('Scaled Hybrid data')
+    if(first.index == 20)
+      return('Scaled Rose data')
+    if(first.index == 21)
       return('Scaled Smote data')
    
-  }
-  else
-  {
-    if(first.index == 1) {
-      if(second.index == 1)
-        return('Simple data')
-      if(second.index == 2)
-        return('Normalized simple data')
-      if(second.index == 3)
-        return('Scaled simple data')
-    }
-    if(first.index == 2) {
-      if(second.index == 1)
-        return('C1Stress modified')
-      if(second.index == 2)
-        return('Normalized C1Stress modified')
-      if(second.index == 3)
-        return('Scaled C1Stress modified')
-    }
-  }
 }
 
 
@@ -75,8 +66,6 @@ datasets.list <- list()
 simple.list <- list()
 
 simple.list[[1]] <- simple.data
-simple.list[[2]] <- normalized.simple.data
-simple.list[[3]] <- scaled.simple.data
 
 # add simple data
 datasets.list[[1]] <- simple.list
@@ -108,38 +97,54 @@ datasets.list[[7]] <- smote.datasets
 
 # add normalized data
 
+# add normalized simple data
+simple.list[[1]] <- normalized.simple.data
+datasets.list[[8]] <- simple.list
+
+# add normalized C1Stress data
+simple.list[[1]] <- normalized.C1Stress.dataset
+datasets.list[[9]] <- simple.list
+
 # add normalized oversampled
-datasets.list[[8]] <- normalized.oversampled.datasets
+datasets.list[[10]] <- normalized.oversampled.datasets
 
 # add normalized undersampled
-datasets.list[[9]] <- normalized.undersampled.datasets
+datasets.list[[11]] <- normalized.undersampled.datasets
 
 # add normalized hybrid
-datasets.list[[10]] <- normalized.hybrid.datasets
+datasets.list[[12]] <- normalized.hybrid.datasets
 
 # add normalized rose 
-datasets.list[[11]] <- normalized.rose.datasets
+datasets.list[[13]] <- normalized.rose.datasets
 
 # add normalized smote
-datasets.list[[12]] <- normalized.smote.datasets
+datasets.list[[14]] <- normalized.smote.datasets
 
 
 # add scaled data
 
+# add scaled simple data
+simple.list[[1]] <- scaled.simple.data
+datasets.list[[15]] <- simple.list
+
+# add scaled C1Stress data
+simple.list[[1]] <- scaled.simple.data
+datasets.list[[16]] <- simple.list
+
 # add scaled oversampled
-datasets.list[[13]] <- scaled.oversampled.datasets
+datasets.list[[17]] <- scaled.oversampled.datasets
 
 # add scaled undersampled
-datasets.list[[14]] <- scaled.undersampled.datasets
+datasets.list[[18]] <- scaled.undersampled.datasets
 
 # add scaled hybrid
-datasets.list[[15]] <- scaled.hybrid.datasets
+datasets.list[[19]] <- scaled.hybrid.datasets
 
 # add scaled rose
-datasets.list[[16]] <- scaled.rose.datasets
+datasets.list[[20]] <- scaled.rose.datasets
 
 # add scaled smote
-datasets.list[[17]] <- scaled.smote.datasets
+datasets.list[[21]] <- scaled.smote.datasets
 
 
 
